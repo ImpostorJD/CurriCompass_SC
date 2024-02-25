e8e19a63898a71ceebaf05b047f290ad31b4907f
akatsu <kevinraymiranda18@gmail.com>
2024-02-25 16:09:38 +0800

    added login-ui

--- CurriCompassUI/src/app/app.component.ts
+++ CurriCompassUI/src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
 import { RouterOutlet } from '@angular/router';
+import { LoginUiComponent } from "./login-ui/login-ui.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,LoginUiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
