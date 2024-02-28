6dda8665cb2ca43635197dc71797701743dbda2d
Paloochie <hendricksjason27@gmail.com>
2024-02-29 07:53:58 +0800

    Student-Form Created

--- CurriCompassUI/src/app/app.component.ts
+++ CurriCompassUI/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { RouterOutlet } from '@angular/router';
 import { LoginUiComponent } from "./login-ui/login-ui.component";
+import { StudentFormComponent } from "./student-form/student-form.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,LoginUiComponent],
+  imports: [CommonModule, RouterOutlet,LoginUiComponent,StudentFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
